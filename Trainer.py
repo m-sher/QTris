cb63de832232d63a63a9eb187d7657bc64ba6657
m-sherrick <michael.a.sherrick@gmail.com>
2024-10-16 00:27:22 -0700

    Add new reward metrics

--- Trainer.py
+++ Trainer.py
@@ -124,8 +124,9 @@ class Trainer():
                 self.replay_buffer.add_batch((board[None, ...], pieces[None, ...], action[None, ...], probs[None, ...], ret[None, ...], advantage[None, ...]))
         
             avg_reward = tf.reduce_mean(episode_rewards)
+            sum_reward = tf.reduce_sum(episode_rewards)
             
-            print(f'\rCurrent Gen: {gen + 1}\t|\tReward: {avg_reward:1.1f}\t|', end='\n', flush=True)
+            print(f'\rCurrent Gen: {gen + 1}\t|\tAvg Reward: {avg_reward:1.1f}\t|\tTotal Reward: {sum_reward:1.1f}\t|', end='\n', flush=True)
         
             # Make dataset sampling from replay buffer
             dset = self.replay_buffer.as_dataset(
@@ -141,9 +142,11 @@ class Trainer():
                 print(f'\rActor Loss: {actor_loss:1.2f}\t|\tCritic Loss: {critic_loss:1.2f}\t|\t', end='', flush=True)
                 wandb.log({'actor_loss': actor_loss,
                            'critic_loss': critic_loss,
-                           'reward': avg_reward})
+                           'reward': sum_reward,
+                           'reward_per_piece': avg_reward})
             else:
                 critic_loss = losses
                 print(f'\rCritic Loss: {critic_loss:1.2f}\t|\t', end='', flush=True)
                 wandb.log({'critic_loss': critic_loss,
-                           'reward': avg_reward})
+                           'reward': sum_reward,
+                           'reward_per_piece': avg_reward})
