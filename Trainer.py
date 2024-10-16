0697487ed7f2e7be0e0f1b2caed8a64df7c28cd8
m-sherrick <michael.a.sherrick@gmail.com>
2024-10-16 12:54:18 -0700

    Add wandb logging

--- Trainer.py
+++ Trainer.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import wandb
 from Player import Player
 from tf_agents.replay_buffers import TFUniformReplayBuffer
 
 class Trainer():
-    def __init__(self, model, optimizers, max_episode_steps=2000, buffer_cap=10000):
+    def __init__(self, model, optimizers, seq_len, gamma, max_episode_steps=2000, buffer_cap=10000):
         self.model = model
         self.actor_optimizer, self.critic_optimizer = optimizers
-        self.player = Player()
+        self.player = Player(max_len=seq_len,
+                             gamma=gamma)
         self.max_episode_steps = max_episode_steps
         self.wandb_run = wandb.init(
             project='Tetris'
@@ -15,8 +17,8 @@ class Trainer():
         
         data_spec = (tf.TensorSpec(shape=(28, 10, 1), dtype=tf.float32, name='Boards'),
              tf.TensorSpec(shape=(7), dtype=tf.int32, name='Pieces'),
-             tf.TensorSpec(shape=(max_len,), dtype=tf.int32, name='ChosenAction'),
-             tf.TensorSpec(shape=(max_len-1, 1), dtype=tf.float32, name='ActionProbs'),
+             tf.TensorSpec(shape=(seq_len,), dtype=tf.int32, name='ChosenAction'),
+             tf.TensorSpec(shape=(seq_len-1, 1), dtype=tf.float32, name='ActionProbs'),
              tf.TensorSpec(shape=(1), dtype=tf.float32, name='Returns'),
              tf.TensorSpec(shape=(1), dtype=tf.float32, name='Advantage'))
 
@@ -30,12 +32,12 @@ class Trainer():
         img = ax.imshow(tf.zeros((28, 10)), vmin=0, vmax=1)
         self.renderer = (fig, img)
     
-    def fill_replay_buffer(self, max_episodes=50, max_episode_steps=self.max_episode_steps):
+    def fill_replay_buffer(self, max_episodes=50):
         for i in range(max_episodes):
-            if replay_buffer.num_frames() >= replay_buffer.capacity:
+            if self.replay_buffer.num_frames() >= self.replay_buffer.capacity:
                 break
         
-            episode_data = self.player.run_episode(self.model, max_steps=max_episode_steps, greedy=False, renderer=self.renderer)
+            episode_data = self.player.run_episode(self.model, max_steps=self.max_episode_steps, greedy=False, renderer=self.renderer)
             episode_boards, episode_pieces, episode_actions, episode_probs, episode_rewards, episode_returns, _, episode_advantages = episode_data
         
             for frame in zip(episode_boards, episode_pieces, episode_actions, episode_probs, episode_returns, episode_advantages):
@@ -99,7 +101,7 @@ class Trainer():
         else:
             return critic_loss
     
-    def train(self, gens, train_steps=100, training_actor=False, max_episode_steps=self.max_episode_steps):
+    def train(self, gens, train_steps=100, training_actor=False):
         self.critic_variables = []
         self.actor_variables = []
         for layer in agent.layers:
@@ -116,7 +118,7 @@ class Trainer():
         
         for gen in range(gens):
             # Run episode
-            episode_data = self.player.run_episode(self.model, max_steps=max_episode_steps, greedy=False, renderer=self.renderer)
+            episode_data = self.player.run_episode(self.model, max_steps=self.max_episode_steps, greedy=False, renderer=self.renderer)
             episode_boards, episode_pieces, episode_actions, episode_probs, episode_rewards, episode_returns, _, episode_advantages = episode_data
         
             for frame in zip(episode_boards, episode_pieces, episode_actions, episode_probs, episode_returns, episode_advantages):
