74451caacb7d36fefc770f85ee3598de77f36e22
m-sherrick <michael.a.sherrick@gmail.com>
2024-10-21 23:07:05 -0700

    Per key values

--- Player.py
+++ Player.py
@@ -34,6 +34,7 @@ class Player():
     def run_episode(self, agent, max_steps=50, greedy=False, renderer=None):
         episode_boards = []
         episode_pieces = []
+        episode_inputs = []
         episode_actions = []
         episode_probs = []
         episode_values = []
@@ -46,55 +47,51 @@ class Player():
             piece_obs = tf.cast(piece, tf.int32)
             inp_seq = tf.cast([[11]], tf.int32)
             key_chars = []
+            
             board_rep, _ = agent.process_board((board_obs[None, ...], piece_obs[None, ...]), training=False)
-            values, _ = agent.process_vals(board_rep, training=False)
+            
             for _ in range(self.max_len-1):
                 logits, _ = agent.process_keys((board_rep, inp_seq), training=False)
+                values, _ = agent.process_vals((board_rep, inp_seq), training=False)
                 
                 if greedy or tf.random.uniform(()) > 0.1:
                     key = tf.argmax(logits[:, -1:], axis=-1, output_type=tf.int32) # (1, 1)
                 else:
                     key = tf.random.categorical(logits[:, -1], num_samples=1, dtype=tf.int32) # (1, 1)
-    
+
+                episode_boards.append(board_obs)
+                episode_pieces.append(piece_obs)
+                episode_inputs.append(self._pad(inp_seq[0], self.max_len))
+                episode_actions.append(key[0])
+                episode_probs.append(tf.nn.log_softmax(logits, axis=-1)[0, -1, tf.squeeze(key)])
+                episode_values.append(values[0, -1])
+                episode_rewards.append(-0.1)
+                
                 inp_seq = tf.concat([inp_seq, key], axis=-1)
-    
                 key = tf.squeeze(key).numpy()
                 key_chars.append(self.key_dict[key])
+                
                 if key == 8:
                     break
-    
-            if key_chars[-1] != 'H':
-                key_chars[-1] = 'H'
-                inp_seq = tf.concat([inp_seq[:, :-1], [[8]]], axis=-1)
-    
-            chosen_prob = tf.gather(tf.nn.log_softmax(logits, axis=-1),
-                                    inp_seq[..., 1:],
-                                    batch_dims=2)
             
             board, piece, reward, terminated = self.game.step(key_chars)
-    
+            episode_rewards[-1] = reward - 0.1
+            
             if renderer:
                 fig, img = renderer
                 img.set_data(board)
                 fig.canvas.draw()
                 fig.canvas.flush_events()
-            
-            episode_boards.append(board_obs)
-            episode_pieces.append(piece_obs)
-            episode_actions.append(self._pad(inp_seq[0], self.max_len))
-            episode_probs.append(self._pad(chosen_prob[0], self.max_len-1))
-            episode_values.append(values[0])
-            episode_rewards.append(reward + self.eps)
     
             if terminated:
                 break
         
         episode_boards = tf.stack(episode_boards, axis=0)
         episode_pieces = tf.stack(episode_pieces, axis=0)
+        episode_inputs = tf.stack(episode_inputs, axis=0)
         episode_actions = tf.stack(episode_actions, axis=0)
-        episode_probs = tf.stack(episode_probs, axis=0)
+        episode_probs = tf.stack(episode_probs, axis=0)[..., None]
         episode_values = tf.stack(episode_values + [[0.0]], axis=0)
         episode_rewards = tf.stack(episode_rewards, axis=0)[..., None]
         
-        return (episode_boards, episode_pieces, episode_actions,
-                episode_probs, episode_values, episode_rewards)
+        return episode_boards, episode_pieces, episode_inputs, episode_actions, episode_probs, episode_values, episode_rewards
