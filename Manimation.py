55a11661e1cbb6ed685acc7cd7010c3e126c1f51
m-sher <michael.a.sherrick@gmail.com>
2024-11-07 02:13:17 -0800

    Update Manimation.py

--- Manimation.py
+++ Manimation.py
@@ -5,7 +5,7 @@ class ModelFlow(Scene):
         # make board
         cells = VGroup(*[Square(side_length=0.25, stroke_color=DARK_GREY) for _ in range(280)])
         cells.arrange_in_grid(rows=28, buff=0.0)
-        self.add(cells)
+        self.play(Write(cells))
         
         # setup tki
         tki = [1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
@@ -14,6 +14,12 @@ class ModelFlow(Scene):
                0, 0, 0, 1, 1, 1, 0, 0, 0, 0]
         tki_cells = VGroup(*[cell for cell, val in zip(cells[::-1], tki) if val])
         
-        self.play(tki_cells.animate(lag_ratio=0.2).set_fill(WHITE, opacity=1.0))
+        self.play(tki_cells.animate(lag_ratio=0.1).set_fill(WHITE, opacity=1.0))
+
+        # make them happen almost simultaneously
+        for i in range(280):
+            conv = cells[i].copy().set_stroke(BLUE, opacity=1.0).save_state().scale(3)
+            self.play(Write(conv))
+            self.play(conv.animate.restore())
         
         self.wait()
