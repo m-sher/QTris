578ff25feb0cbfdc7c9ba12e1bb8fbb8752a4636
m-sherrick <michael.a.sherrick@gmail.com>
2024-11-08 00:04:40 -0800

    Update Manimation.py

--- Manimation.py
+++ Manimation.py
@@ -1,25 +1,41 @@
 from manim import *
+import numpy as np
 
 class ModelFlow(Scene):
     def construct(self):
-        # make board
-        cells = VGroup(*[Square(side_length=0.25, stroke_color=DARK_GREY) for _ in range(280)])
-        cells.arrange_in_grid(rows=28, buff=0.0)
-        self.play(Write(cells))
-        
-        # setup tki
-        tki = [1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
-               1, 0, 0, 0, 1, 1, 1, 1, 1, 1,
-               1, 0, 0, 1, 1, 1, 1, 0, 0, 0,
-               0, 0, 0, 1, 1, 1, 0, 0, 0, 0]
-        tki_cells = VGroup(*[cell for cell, val in zip(cells[::-1], tki) if val])
-        
-        self.play(tki_cells.animate(lag_ratio=0.1).set_fill(WHITE, opacity=1.0))
 
-        # make them happen almost simultaneously
-        for i in range(280):
-            conv = cells[i].copy().set_stroke(BLUE, opacity=1.0).save_state().scale(3)
-            self.play(Write(conv))
-            self.play(conv.animate.restore())
+        border = Rectangle(height=7, width=2.5, grid_xstep=0.25, grid_ystep=0.25)
+        border.grid_lines.set_stroke(width=2, color=DARK_GREY)
+
+        np_tki = (np.load('tki.npy') * 255).astype(np.uint8)
+        tki = ImageMobject(np_tki).set_resampling_algorithm(RESAMPLING_ALGORITHMS["nearest"])
+        tki.fade(1.0).match_height(border)
+        self.add(tki)
+
+        np_conv1 = (np.load('conv1.npy') * 255).astype(np.uint8)
+        conv1 = ImageMobject(np_conv1).set_resampling_algorithm(RESAMPLING_ALGORITHMS["nearest"])
+        conv1.fade(1.0).match_height(border)
+        self.add(conv1)
+
+        np_conv2 = (np.load('conv2.npy') * 255).astype(np.uint8)
+        conv2 = ImageMobject(np_conv2).set_resampling_algorithm(RESAMPLING_ALGORITHMS["nearest"])
+        conv2.fade(1.0).match_height(border)
+        self.add(conv2)
+
+        np_conv3 = (np.load('conv3.npy') * 255).astype(np.uint8)
+        conv3 = ImageMobject(np_conv3).set_resampling_algorithm(RESAMPLING_ALGORITHMS["nearest"])
+        conv3.fade(1.0).match_height(border)
+        self.add(conv3)
         
+        self.wait()
+
+        self.play(Write(border))
+        
+        self.play(tki.animate.fade(0.0))
+        self.wait()
+        self.play(conv1.animate.fade(0.0))
+        self.wait()
+        self.play(conv2.animate.fade(0.0))
+        self.wait()
+        self.play(conv3.animate.fade(0.0), Transform(border, Rectangle(height=7, width=2.5, grid_xstep=0.5, grid_ystep=0.5)))
         self.wait()
