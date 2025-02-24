bc5be8a45ddcf32ee3b85c9143ca0fdd9dc3379f
m-sher <michael.a.sherrick@gmail.com>
2025-02-23 19:57:15 -0800

    Reduce to only necessary moves (still 2 extra to remove)

--- GenerateMoves.py
+++ GenerateMoves.py
@@ -3,22 +3,18 @@ def generate_all_finesse_moves_dict():
     
     # Define option lists (order matters)
     possible_holds = ["", "h"]
-    standard_horizontal = ["", "L", "R", "Lr", "Rl", "ll", "rr", "l", "r"]
-    standard_rotations = ["", "c", "a", "1"]
+    
+    standard_moves = ["L", "Lr", "ll", "l", "", "r", "rr", "Rl", "R",
+                      "L1", "ll1", "l1", "1", "r1", "rr1", "Rl1", "R1",
+                      "Lc", "llc", "lc", "c", "rc", "rrc", "Rlc", "Rc",
+                      "La", "lla", "la", "a", "ra", "rra", "Rla", "Ra",
+                      "cL", "cR", "aL", "aR"]
+                    
     spin_moves = ["", "c", "a", "1", "cc", "aa", "c1", "a1"]
-
-    standard_moves = []
-    for h in standard_horizontal:
-        for r in standard_rotations:
-            standard_moves.append(h+r)
-    for h in standard_horizontal:
-        for r in standard_rotations:
-            if r+h not in standard_moves:
-                standard_moves.append(r+h)
 
     # This dictionary will hold the mapping:
     # key: move string (ending with "H")
-    # value: [hold, standard, slide, spin]
+    # value: (hold, standard, spin)
     str_to_ind = {}
     ind_to_str = {}
     
@@ -34,23 +30,3 @@ def generate_all_finesse_moves_dict():
 
 # str_to_ind, ind_to_str = generate_all_finesse_moves_dict()
 # print(len(str_to_ind))
-
-def generate_key_dict():
-    ind_to_str = {
-        0: 'S',
-        1: 'h',
-        2: 'l',
-        3: 'r',
-        4: 'L',
-        5: 'R',
-        6: 'a',
-        7: 'c',
-        8: '1',
-        9: 's',
-        10: 'H',
-        11: 'P'
-    }
-    
-    str_to_ind = {val: key for key, val in ind_to_str.items()}
-    
-    return ind_to_str, str_to_ind
