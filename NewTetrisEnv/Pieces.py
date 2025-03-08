242eae726de64d25fd4091ca3cff103fff554c19
m-sherrick <michael.a.sherrick@gmail.com>
2025-03-07 18:45:48 -0800

    Add new environment

---
+++ NewTetrisEnv/Pieces.py
@@ -0,0 +1,28 @@
+import numpy as np
+from enum import Enum
+
+class PieceType(Enum):
+    N = 0 # NULL - empty and only used for initial hold
+    I = 1
+    J = 2
+    L = 3
+    O = 4
+    S = 5
+    T = 6
+    Z = 7
+    
+class Piece:
+    def __init__(self, piece_type: PieceType = PieceType.N,
+                 loc: np.ndarray = np.zeros((2,), dtype=np.int32),
+                 r: int = 0, cells: np.ndarray = np.zeros((4, 2), dtype=np.int32)):
+        
+        self.piece_type = piece_type
+        
+        self.loc = loc
+        self.delta_loc = np.zeros((2,), np.int32)
+        
+        self.r = r
+        self.delta_r = 0
+        
+        self.cells = cells
+        
---
+++ NewTetrisEnv/README.md
@@ -0,0 +1,13 @@
+# TensorFlow-Tetris-SRS-Plus
+TensorFlow environment for RL with Tetris SRS+
+
+## Todo
+- Undo for debugging
+- Supplemental scoring
+- More spins
+	- Immobile detection for all-mini+
+- Combo table
+- Everything else :)
+
+### Done
+- ~~Standard scoring~~
