aa3400c8efc0ef3468f5010e08155d91ccfbff4d
m-sher <michael.a.sherrick@gmail.com>
2025-06-25 21:56:20 -0700

    Make demo runner interactive

--- RunDemo.py
+++ RunDemo.py
@@ -3,6 +3,8 @@ from TetrisModel import PolicyModel
 from TetrisEnvs.PyTetrisEnv.PyTetrisEnv import PyTetrisEnv
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
 import pygame
+import pygame_widgets
+from pygame_widgets.slider import Slider
 import imageio
 import numpy as np
 import time
@@ -17,7 +19,7 @@ num_layers = 4
 dropout_rate = 0.1
 max_len = 9
 
-num_steps = 500
+num_steps = 100
 queue_size = 5
 max_holes = 10
 max_height = 20
@@ -33,7 +35,7 @@ p_model = PolicyModel(batch_size=num_envs,
                       output_dim=key_dim)
 
 p_checkpoint = tf.train.Checkpoint(model=p_model)
-p_checkpoint_manager = tf.train.CheckpointManager(p_checkpoint, './policy_checkpoints', max_to_keep=3)
+p_checkpoint_manager = tf.train.CheckpointManager(p_checkpoint, './policy_checkpoints_6', max_to_keep=3)
 p_checkpoint.restore(p_checkpoint_manager.latest_checkpoint).expect_partial()
 
 p_model.build(input_shape=[(None, 24, 10, 1),
@@ -53,16 +55,22 @@ env = TFPyEnvironment(py_env)
 pygame.init()
 screen = pygame.display.set_mode((625, 600))
 pygame.display.set_caption("Tetris")
+font = pygame.font.Font(None, 30)
 
 time_step = env.reset()
 
 frames = []
+attacks = []
+clears = []
+
 piece_display = np.load('PieceDisplay.npy')
 
 start = time.time()
 for t in range(num_steps):
     board = time_step.observation['board']
     pieces = time_step.observation['pieces']
+    attack = time_step.reward['attack']
+    clear = time_step.reward['clear']
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -95,9 +103,37 @@ for t in range(num_steps):
     pygame.display.update()
 
     frames.append(pygame.surfarray.array3d(screen).swapaxes(0, 1))
+    attacks.append(attack.numpy()[0])
+    clears.append(clear.numpy()[0])
 
     time_step = env.step(key_sequence)
 
 print(f"Time taken: {time.time() - start:.2f} seconds")
 
-imageio.mimsave('demo.gif', frames, fps=10)
+# imageio.mimsave('demo.gif', frames, fps=3)
+
+slider = Slider(screen, x=10, y=5, width=600, height=10, min=0, max=num_steps - 1, step=1, colour=(125, 125, 125), handleColour=(50, 50, 50))
+
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.fill((255, 255, 255))
+
+    ind = slider.getValue()
+    frame = frames[ind]
+
+    pygame.surfarray.blit_array(screen, frame.swapaxes(0, 1))
+    pygame_widgets.update(events)
+    
+    attack_text = font.render(f"Attack: {attacks[ind]}", True, (255, 255, 255))
+    clear_text = font.render(f"Clear: {int(clears[ind] * 5)}", True, (255, 255, 255))
+    
+    # Position text below the slider
+    screen.blit(attack_text, (10, 35))
+    screen.blit(clear_text, (10, 70))
+
+    pygame.display.update()
