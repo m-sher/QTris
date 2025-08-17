211e9b8ed6987be00080afce020895e721dfd59a
Michael Sherrick <michael.a.sherrick@gmail.com>
2025-08-17 00:13:24 -0700

    Refactor to separate Oneshot from Autoregressive

---
+++ Oneshot/Pretrainer.py
@@ -0,0 +1,320 @@
+from TetrisEnvs.PyTetrisEnv.PyTetrisEnv import PyTetrisEnv
+from TetrisEnvs.PyTetrisEnv.Moves import Moves, Convert
+from TetrisEnvs.PyTetrisEnv.Pieces import PieceType
+from TetrisModel import TetrisModel
+import multiprocessing
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import glob
+from tqdm import tqdm
+import time
+import os
+
+class Pretrainer():
+    def __init__(self):
+        self._new_pieces = {
+            0: PieceType.N.value,
+            1: PieceType.I.value,
+            2: PieceType.T.value,
+            3: PieceType.L.value,
+            4: PieceType.J.value,
+            5: PieceType.Z.value,
+            6: PieceType.S.value,
+            7: PieceType.O.value,
+        }
+        self.scc = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    def _load_raw_data(self):
+        """
+        Loads the raw game data from text files.
+        """
+        raw_data = [[], []]
+        for file in glob.glob('E:\\MisaMino-Tetrio\\MisaMino\\tetris_ai\\logs\\game*.txt'):
+            with open(file) as f:
+                contents = f.readlines()
+                for line in contents:
+                    raw_data[int(line[0])].append(line[2:])
+            print(f"Loaded {len(contents)} lines from {file}", flush=True)
+        return raw_data
+
+    def _process_into_transitions(self, raw_data: list[list[str]]) -> list[tuple[tuple[np.ndarray, np.ndarray],
+                                                                                 tuple[np.ndarray, np.ndarray]]]:
+        """
+        Process the raw game data into transitions of ((state), (next_state)).
+        Raw game data has the following fields in order, separated by '#':
+        1. Player ID (removed by `_load_raw_data`)
+        2. Active piece
+        3. Hold piece
+        4. Queue of next pieces
+        5. Board (represented by a list of integers where each integer is
+                  equivalent to a binary representation of the corresponding row)
+        """
+        transitions = []
+        transitions_checked = 0
+        last_time = time.time()
+        for player_data in raw_data:
+            for i in range(len(player_data) - 1):
+
+                # Parse state
+                split_results = player_data[i].strip().split('#')
+                active = int(split_results[0])
+                hold = int(split_results[1])
+                queue = [int(piece) for piece in split_results[2].split(',')[:5]]
+                piece_seq = np.array([self._new_pieces[piece] for piece in [active] + [hold] + queue], dtype=np.int32)
+                board = np.array([[int(bit) for bit in '{:032b}'.format(int(row))[-10:][::-1]] for row in split_results[3].split(',')[5:-4]], dtype=np.int32)
+                state = (board, piece_seq)
+
+                # Parse next_state
+                next_split_results = player_data[i + 1].strip().split('#')
+                next_active = int(next_split_results[0])
+                next_hold = int(next_split_results[1])
+                next_queue = [int(piece) for piece in next_split_results[2].split(',')[:5]]
+                next_piece_seq = np.array([self._new_pieces[piece] for piece in [next_active] + [next_hold] + next_queue], dtype=np.int32)
+                next_board = np.array([[int(bit) for bit in '{:032b}'.format(int(row))[-10:][::-1]] for row in next_split_results[3].split(',')[5:-4]], dtype=np.int32)
+                next_state = (next_board, next_piece_seq)
+
+                transitions_checked += 1
+
+                if self._is_candidate(state, next_state):
+                    transitions.append((state, next_state))
+
+                if time.time() - last_time > 5:
+                    print(f"\rtransitions checked: {transitions_checked} | Valid transitions: {len(transitions)}", end='', flush=True)
+                    last_time = time.time()
+
+        print(f"\rTotal transitions checked: {transitions_checked} | Total valid transitions: {len(transitions)}", flush=True)
+
+        return transitions
+    
+    def _is_candidate(self, state: tuple[np.ndarray, np.ndarray], next_state: tuple[np.ndarray, np.ndarray]) -> bool:
+        """
+        Determine if the transition from board_t to board_t1 is a candidate for training.
+        This removes transitions between two episodes and transitions that accept garbage.
+        """
+        board, piece_seq = state
+        next_board, next_piece_seq = next_state
+
+        # Check that the queue cycles as expected
+        # Piece sequence is [active, hold, next1, next2, next3, next4, next5]
+        if not (np.array_equal(piece_seq[3:], next_piece_seq[2:-1]) or # Queue cycled once
+                (piece_seq[1] == 0 and # Hold started empty
+                 next_piece_seq[1] != 0 and # Hold is now occupied
+                 np.array_equal(piece_seq[4:], next_piece_seq[2:-2]))): # Queue cycled twice
+            return False
+
+        # Check that the board changes in an expected way
+        cell_count_diff = np.sum(next_board) - np.sum(board)
+        if not (cell_count_diff == 4 or # Placed a piece without clearing lines
+                cell_count_diff == 4 - 10 or # Placed a piece and cleared one line
+                cell_count_diff == 4 - 20 or # Placed a piece and cleared two lines
+                cell_count_diff == 4 - 30 or # Placed a piece and cleared three lines
+                cell_count_diff == 4 - 40): # Placed a piece and cleared four lines
+            return False
+        
+        return True
+    
+    def _find_action(self, transition: tuple[tuple[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray]],
+                     test_env: PyTetrisEnv) -> tuple[int, int, int]:
+        """
+        Determine the action taken to transition from state_t to state_t1.
+        """
+        state, next_state = transition
+
+        board, piece_seq = state
+        next_board, next_piece_seq = next_state
+
+        # Determine whether hold was used
+        if piece_seq[1] != next_piece_seq[1]:
+            hold = 1
+        else:
+            hold = 0
+
+        # Determine the action(s) that cause the transition
+        # Checking non-spins first to avoid unnecessary keypresses with spins
+        for spin in range(len(Moves._spins)):
+            for standard in range(len(Moves._standards)):
+                action = {
+                    'hold': hold,
+                    'standard': standard,
+                    'spin': spin
+                }
+                active_piece = test_env._spawn_piece(PieceType(piece_seq[0]))
+                hold_piece = PieceType(piece_seq[1])
+                queue = [PieceType(piece) for piece in piece_seq[2:]]
+                _, _, sim_board, _, _, _ = test_env._execute_action(board, active_piece,
+                                                                    hold_piece, queue, action)
+                if np.array_equal(sim_board, next_board):
+                    return board, piece_seq, (hold, standard, spin)
+        
+        # If no action is found, return action to filter
+        return board, piece_seq, (-1, -1, -1)
+
+    def process_single_transition(self, transition):
+        return self._find_action(transition, self.test_env)
+
+    def _generate_dataset(self) -> tf.data.Dataset:
+        """
+        Generate a dataset of transitions from the raw game data.
+        """
+
+        # Initialize test environment
+        self.test_env = PyTetrisEnv(queue_size=5, max_holes=100, seed=123, idx=0)
+
+        # Load transitions
+        raw_data = self._load_raw_data()
+        transition_df = self._process_into_transitions(raw_data)
+        
+        with multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1) as pool:
+            results = list(tqdm(
+                pool.imap(self.process_single_transition, transition_df, chunksize=512),
+                total=len(transition_df),
+                desc="Processing transitions",
+            ))
+
+        # Convert results from list of tuples to tuple of lists
+        repacked = tuple(map(list, zip(*results)))
+
+        # Create dataset
+        dataset = (tf.data.Dataset.from_tensor_slices(repacked)
+                   .filter(lambda board, piece_seq, action: tf.reduce_all(action[1] != (-1, -1, -1))))
+
+        # Save dataset and ensure it can be loaded
+        dataset.save('tetris_expert_dataset')
+        dataset = tf.data.Dataset.load('tetris_expert_dataset')
+
+        return dataset
+
+    @tf.function
+    def _get_action_ind(self, separate_action):
+        action = tf.gather_nd(Convert.to_ind,
+                              separate_action)
+        return action
+        
+
+    def _load_dataset(self, batch_size: int | None=1024) -> tf.data.Dataset:
+        """
+        Load the dataset from disk if it exists, otherwise generate it.
+        """
+
+        # Check if dataset exists
+        if not os.path.exists('../tetris_expert_dataset'):
+            if input("Dataset not found. Generate? (y/n)") == 'y':
+                # Generate dataset
+                dataset = self._generate_dataset()
+            else:
+                raise FileNotFoundError("Dataset not found and user did not want to generate it.")
+        else:
+            try:
+                dataset = tf.data.Dataset.load('../tetris_expert_dataset')
+                print("Dataset loaded successfully.", flush=True)
+            except:
+                print("Dataset loading failed. Generating dataset...", flush=True)
+                # Generate dataset
+                dataset = self._generate_dataset()
+
+        dataset = (dataset
+                   .map(lambda board, piece_seq, separate_action: {
+                            'boards': tf.cast(board[..., None], tf.float32),
+                            'pieces': tf.cast(piece_seq, tf.int64),
+                            'actions': tf.cast(self._get_action_ind(separate_action), tf.int64)
+                        },
+                        num_parallel_calls=tf.data.AUTOTUNE,
+                        deterministic=False)
+                   .cache()
+                   .shuffle(1000000))
+
+        if batch_size:
+            dataset = (dataset
+                       .batch(batch_size,
+                              deterministic=False,
+                              drop_remainder=True,
+                              num_parallel_calls=tf.data.AUTOTUNE)
+                       .prefetch(tf.data.AUTOTUNE))
+        
+        return dataset
+
+    @tf.function
+    def _train_step(self, model: keras.Model, batch) -> tuple[float, float]:
+        """
+        Perform a single training step.
+        """
+        board = batch['boards']
+        piece_seq = batch['pieces']
+        target_action = batch['actions']
+
+        with tf.GradientTape() as tape:
+            # Forward pass
+            logits = model((board, piece_seq), training=True)
+            # Compute loss
+            loss = self.scc(target_action, logits)
+        # Compute and apply gradients
+        gradients = tape.gradient(loss, model.trainable_variables)
+        model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        # Compute accuracy
+        accuracy = tf.reduce_mean(tf.cast(tf.argmax(logits, axis=-1, output_type=tf.int64) == target_action, tf.float32))
+
+        return loss, accuracy
+
+    def train(self, model: keras.Model, epochs: int=10, batch_size: int=1024, checkpoint_manager=None):
+        """
+        Train the model on saved dataset.
+        """
+        # Load dataset
+        dataset = self._load_dataset(batch_size)
+
+        # Train model
+        for epoch in range(epochs):
+            print(f"Epoch {epoch + 1}/{epochs}", flush=True)
+            for step, batch in enumerate(dataset):
+                # Perform training step
+                loss, accuracy = self._train_step(model, batch)
+                # Print progress every 100 steps
+                if step % 100 == 0:
+                    print(f"Step {step + 1} | Loss: {loss:2.3f} | Accuracy: {accuracy:1.3f}", flush=True)
+            # Save checkpoint after each epoch
+            if checkpoint_manager is not None:
+                checkpoint_manager.save()
+
+def main():
+    # Model params
+    piece_dim = 8
+    num_actions = 560
+    depth = 64
+    num_heads = 4
+    num_layers = 6
+    dropout_rate = 0.1
+    batch_size = 256
+
+    queue_size = 5
+
+    # Initialize model and optimizer
+    model = TetrisModel(piece_dim=piece_dim,
+                        depth=depth,
+                        num_heads=num_heads,
+                        num_layers=num_layers,
+                        dropout_rate=dropout_rate,
+                        output_dim=num_actions)
+    
+    optimizer = keras.optimizers.Adam(1e-4)
+    model.compile(optimizer=optimizer, jit_compile=True)
+    print("Initialized model and optimizer.", flush=True)
+
+    model.build(input_shape=[(None, 24, 10, 1),
+                             (None, queue_size + 2)])
+    
+    model.summary()
+    input()
+    # Initialize checkpoint manager
+    checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer)
+    checkpoint_manager = tf.train.CheckpointManager(checkpoint, './pretrained_checkpoints', max_to_keep=3)
+    # checkpoint.restore(checkpoint_manager.latest_checkpoint)
+    # checkpoint_manager = tf.train.CheckpointManager(checkpoint, '../checkpoints/pretrained_checkpoints', max_to_keep=3)
+    print("Initialized checkpoint manager.", flush=True)
+
+    pretrainer = Pretrainer()
+    pretrainer.train(model, batch_size=batch_size, epochs=10, checkpoint_manager=checkpoint_manager)
+
+if __name__ == "__main__":
+    main()
---
+++ Oneshot/TetrisModel.py
@@ -0,0 +1,244 @@
+import tensorflow as tf
+import keras
+from keras import layers
+from tensorflow_probability import distributions
+from TetrisEnvs.TFTetrisEnv.TFMoves import TFConvert
+
+@tf.function(jit_compile=True)
+def positional_encoding(max_length, depth):
+    half_depth = depth / 2
+    positions = tf.range(max_length, dtype=tf.float32)[..., None]
+    depths = tf.range(half_depth, dtype=tf.float32)[None, ...] / half_depth
+
+    angle_rates = 1.0 / (10000 ** depths)
+    angle_rads = positions * angle_rates
+
+    pos_encoding = tf.concat([tf.sin(angle_rads), tf.cos(angle_rads)], axis=-1)
+    return pos_encoding
+
+class PosEncoding(layers.Layer):
+    def __init__(self, depth, max_length):
+        super().__init__()
+        self.depth = depth
+        
+        self.pos_encoding = positional_encoding(max_length, depth)
+        
+        self.add = tf.keras.layers.Add()
+
+    @tf.function(jit_compile=True)
+    def call(self, seq):
+        # Already embedded seq
+        seq_len = tf.shape(seq)[1]
+        
+        seq *= tf.cast(self.depth, tf.float32) ** 0.5
+        seq += self.pos_encoding[None, :seq_len]
+    
+        return seq
+
+class SelfAttention(layers.Layer):
+    def __init__(self, causal, **kwargs):
+        super().__init__()
+        self.causal = causal
+        self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        self.add = tf.keras.layers.Add() 
+        self.layernorm = tf.keras.layers.LayerNormalization()
+
+    @tf.function(jit_compile=True)
+    def call(self, x, training=False):
+        attn, attention_scores = self.mha(query=x, value=x,
+                                          use_causal_mask=self.causal,
+                                          return_attention_scores=True)
+        x = self.add([x, attn])
+        return self.layernorm(x, training=training), attention_scores
+
+class CrossAttention(layers.Layer):
+    def __init__(self,**kwargs):
+        super().__init__()
+        self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        self.add = tf.keras.layers.Add() 
+        self.layernorm = tf.keras.layers.LayerNormalization()
+
+    @tf.function(jit_compile=True)
+    def call(self, x, y, training=False, **kwargs):
+        attn, attention_scores = self.mha(
+            query=x, value=y,
+            return_attention_scores=True)
+        
+        x = self.add([x, attn])
+        return self.layernorm(x, training=training), attention_scores
+
+class FeedForward(layers.Layer):
+    def __init__(self, units, dropout_rate=0.1):
+        super().__init__()
+        self.seq = tf.keras.Sequential([
+            tf.keras.layers.Dense(units=2*units, activation='relu'),
+            tf.keras.layers.Dense(units=units),
+            tf.keras.layers.Dropout(rate=dropout_rate),
+        ])
+        
+        self.layernorm = tf.keras.layers.LayerNormalization()
+
+    @tf.function(jit_compile=True)
+    def call(self, x, training=False):
+        x = x + self.seq(x, training=training)
+        return self.layernorm(x, training=training)
+
+class EncoderLayer(layers.Layer):
+    def __init__(self, units, num_heads=1, dropout_rate=0.1, name='Encoder'):
+        super().__init__(name=name)
+        
+        self.self_attention = SelfAttention(causal=False,
+                                            num_heads=num_heads,
+                                            key_dim=units,
+                                            dropout=dropout_rate)
+        self.ff = FeedForward(units=units, dropout_rate=dropout_rate)
+
+    @tf.function(jit_compile=True)
+    def call(self, inputs, training=False):
+        in_seq = inputs
+        
+        out_seq, _ = self.self_attention(in_seq, training=training)
+        
+        out_seq = self.ff(out_seq, training=training)
+        
+        return out_seq
+    
+class DecoderLayer(layers.Layer):
+    def __init__(self, units, causal, num_heads=1, dropout_rate=0.1, name='Decoder'):
+        super().__init__(name=name)
+        
+        self.self_attention = SelfAttention(causal=causal,
+                                            num_heads=num_heads,
+                                            key_dim=units,
+                                            dropout=dropout_rate)
+        self.cross_attention = CrossAttention(num_heads=num_heads,
+                                              key_dim=units,
+                                              dropout=dropout_rate)
+        self.ff = FeedForward(units=units, dropout_rate=dropout_rate)
+    
+    @tf.function(jit_compile=True)
+    def call(self, inputs, training=False):
+        in_seq, out_seq = inputs
+        
+        out_seq, _ = self.self_attention(out_seq, training=training)
+        
+        out_seq, attn_scores = self.cross_attention(out_seq, in_seq, training=training)
+        
+        out_seq = self.ff(out_seq, training=training)
+        
+        return out_seq, attn_scores
+
+class TetrisModel(keras.Model):
+    def __init__(self, piece_dim, depth, num_heads, num_layers, dropout_rate, output_dim):
+        super().__init__()
+
+        self._depth = depth
+        self._output_dim = output_dim
+        self._num_layers = num_layers
+
+        self.make_patches = keras.Sequential([
+            keras.Input(shape=(24, 10, 1)),
+            layers.Rescaling(scale=2.0, offset=-1.0),
+            layers.Conv2D(filters=depth//2, kernel_size=3, strides=1, padding='same', activation='relu'),
+            layers.Conv2D(filters=depth, kernel_size=3, strides=1, padding='same', activation='relu'),
+            layers.Conv2D(filters=depth, kernel_size=2, strides=2, padding='valid', activation='relu'),
+            layers.Reshape((-1, depth))
+        ])
+        
+        num_patches = self.make_patches.output_shape[1]
+        self.patch_pos_encoding = PosEncoding(
+            depth=depth,
+            max_length=num_patches
+        )
+        
+        self.board_encoder_layers = [EncoderLayer(units=depth, num_heads=num_heads, dropout_rate=dropout_rate, name=f'board_enc_{i}')
+                                     for i in range(num_layers)]
+        
+        self.piece_embedding = layers.Embedding(
+            input_dim=piece_dim,
+            output_dim=depth,
+        )
+        
+        self.piece_pos_encoding = PosEncoding(
+            depth=depth,
+            max_length=7,
+        )
+        
+        self.piece_decoder_layers = [DecoderLayer(units=depth, causal=False, num_heads=num_heads, dropout_rate=dropout_rate, name=f'piece_dec_{i}')
+                                     for i in range(num_layers)]
+
+        self.trunk = keras.Sequential([
+            layers.Flatten(),
+            layers.Dropout(dropout_rate),
+            layers.Dense(depth * 4, activation='relu')
+        ], name='trunk')
+
+        self.top = layers.Dense(output_dim)
+
+    @tf.function(jit_compile=True)
+    def process_obs(self, inputs, training=False):
+        
+        board, piece = inputs
+        
+        piece_scores = []
+        patches = self.make_patches(board, training=training)
+        board_enc = self.patch_pos_encoding(patches)
+        
+        for board_enc_layer in self.board_encoder_layers:
+            board_enc = board_enc_layer(board_enc, training=training)
+        
+        piece_embedding = self.piece_embedding(piece, training=training)
+        piece_dec = self.piece_pos_encoding(piece_embedding)
+        
+        for piece_dec_layer in self.piece_decoder_layers:
+            piece_dec, last_attn = piece_dec_layer([board_enc, piece_dec], training=training)
+            piece_scores.append(last_attn)
+        
+        return piece_dec, piece_scores
+
+    @tf.function(jit_compile=True)
+    def call(self, inputs, training=False, return_scores=False):
+        
+        board, piece = inputs
+        
+        piece_dec, piece_scores = self.process_obs((board, piece), training=training)
+
+        trunk_out = self.trunk(piece_dec, training=training)
+
+        output = self.top(trunk_out, training=training)
+
+        if return_scores:
+            return output, piece_scores
+        else:
+            return output
+
+    @tf.function(jit_compile=True)
+    def predict(self, inputs, return_scores=False, greedy=False):
+
+        board, piece = inputs
+        
+        piece_dec, piece_scores = self.process_obs((board, piece), training=False)
+
+        trunk_out = self.trunk(piece_dec, training=False)
+
+        output = self.top(trunk_out, training=False)
+        
+        if self._output_dim > 1:
+            dist = distributions.Categorical(logits=output, dtype=tf.int64)
+            
+            if greedy:
+                action = tf.argmax(output, axis=-1, output_type=tf.int64)
+            else:
+                action = dist.sample()
+            
+            log_prob = dist.log_prob(action)
+
+            if return_scores:
+                return action, log_prob, piece_scores
+            else:
+                return action, log_prob
+        else:
+            if return_scores:
+                return output, piece_scores
+            else:
+                return output
